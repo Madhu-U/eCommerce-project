34c5a2ae547af36937e861ce89e0be847aa2e20d
Madhu U <umadhu.20@gmail.com>
2025-02-18 19:17:47 +0530

    Added Payments page and modified the Cart UI

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,13 +1,15 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { ProductContext } from "../../context/ProductContext";
 import CartItem from "../../components/CartItem/CartItem";
 import styles from "./cart.module.css";
 import { useNavigate } from "react-router-dom";
+import Payment from "../Payment/Payment";
+import { render } from "react-dom";
 
 const Cart = () => {
   const { cart } = useContext(ProductContext);
+  const [flag, setFlag] = useState(false);
 
-  const navigate = useNavigate();
   let findTotal = () => {
     let total = 0;
     for (let i = 0; i < cart.length; i++) {
@@ -16,48 +18,61 @@ const Cart = () => {
     return Math.round(total * 87.62);
   };
 
-  return (
-    <div className={styles.cart_container}>
-      <div className={styles.cart_left}>
-        <div className={styles.cart_items}>
-          <h1>Shopping Cart</h1>
-          <p className={styles.productCount}>
-            <span>{cart.length}</span> items in your bag.
-          </p>
-          {findTotal() === 0 && <h2>Your Cart is Empty</h2>}
+  const renderPayments = () => {
+    setFlag(!flag);
+  };
 
-          <div className={styles.shoppingCart}>
-            {cart.map((product) => {
-              return (
-                <CartItem productData={product} key={product.id}></CartItem>
-              );
-            })}
+  return (
+    <div>
+      <div className={styles.cart_container}>
+        <div className={styles.cart_left}>
+          <div className={styles.cart_items}>
+            <h1>Shopping Cart</h1>
+            <p className={styles.productCount}>
+              <span>{cart.length}</span> items in your bag.
+            </p>
+            {findTotal() === 0 && <h2>Your Cart is Empty</h2>}
+
+            <div className={styles.shoppingCart}>
+              {cart.map((product) => {
+                return (
+                  <CartItem productData={product} key={product.id}></CartItem>
+                );
+              })}
+            </div>
           </div>
         </div>
-      </div>
-      <div className={styles.cart_right}>
-        <h2>Cart Summary</h2>
-        <div className={styles.cartSummary}>
-          {cart.map((x, i) => {
-            return (
-              <div key={i} className={styles.summaryItem}>
-                <p>{x.title}</p>
-                <p>x 1</p>
-                <p>{`₹ ${Math.round(x.price * 87.62)}`}</p>
+        {cart.length > 0 && (
+          <div className={styles.cart_right}>
+            <h2>Cart Summary</h2>
+            <div className={styles.cartSummary}>
+              {cart.map((x, i) => {
+                return (
+                  <div key={i} className={styles.summaryItem}>
+                    <p>{x.title}</p>
+                    <p>x 1</p>
+                    <p>{`₹ ${Math.round(x.price * 87.62)}`}</p>
+                  </div>
+                );
+              })}
+            </div>
+            {findTotal() > 0 && (
+              <div className={styles.total}>
+                <h2>Total :</h2>
+                <p>{`₹ ${findTotal()}`}</p>
               </div>
-            );
-          })}
-        </div>
-        {findTotal() > 0 && (
-          <div className={styles.total}>
-            <h2>Total :</h2>
-            <p>{`₹ ${findTotal()}`}</p>
+            )}
+            {findTotal() > 0 && (
+              <button className={styles.pay} onClick={renderPayments}>
+                Click to Pay
+              </button>
+            )}
           </div>
         )}
-        {findTotal() > 0 && (
-          <button className={styles.pay} onClick={() => navigate("/payment")}>
-            Click to Pay
-          </button>
+        {flag && (
+          <div className={styles.overlay} onClick={renderPayments}>
+            <Payment state={flag} change={setFlag}></Payment>
+          </div>
         )}
       </div>
     </div>
--- src/pages/Payment/Payment.jsx
+++ src/pages/Payment/Payment.jsx
@@ -1,7 +1,107 @@
-import React from "react";
+import React, { useContext } from "react";
+import styles from "./payment.module.css";
+import { ProductContext } from "../../context/ProductContext";
+import cross from "../../assets/images/x-solid.svg";
 
-const Payment = () => {
-  return <div>Payment</div>;
+const Payment = ({ state, change }) => {
+  const { cart } = useContext(ProductContext);
+
+  let findTotal = () => {
+    let total = 0;
+    for (let i = 0; i < cart.length; i++) {
+      total += cart[i].price;
+    }
+    return Math.round(total * 87.62);
+  };
+
+  return (
+    <div
+      className={styles.paymentContainer}
+      onClick={(e) => e.stopPropagation()}
+    >
+      <div className={styles.address}>
+        <h1>Address</h1>
+        <h2>Enter your Address</h2>
+        <form autoComplete="on">
+          <input
+            type="text"
+            name="name"
+            id="name"
+            required
+            placeholder="Full Name"
+          />
+          <input
+            type="text"
+            name="address"
+            id="address"
+            required
+            placeholder="Address"
+          />
+          <div className={styles.addressFields}>
+            <input
+              type="text"
+              name="city"
+              id="city"
+              placeholder="City"
+              required
+            />
+          </div>
+          <input
+            type="text"
+            name="pincode"
+            id="pincode"
+            placeholder="Pincode"
+            required
+          />
+          <input
+            type="tel"
+            name="contact"
+            id="contact"
+            placeholder="Contact"
+            required
+          />
+          <input type="submit" value="Confirm Address" />
+        </form>
+      </div>
+      <div className={styles.paymentOptions}>
+        <h1>Payment</h1>
+
+        <h2>Payment Options</h2>
+        <p>Choose your Payment Method</p>
+        <div className={styles.radioGroup}>
+          <input type="radio" name="payment_option" id="cards" />
+          <label htmlFor="cards">Debit/Credit Card</label>
+          <input type="radio" name="payment_option" id="upi" />
+          <label htmlFor="upi">UPI</label>
+          <input type="radio" name="payment_option" id="netBanking" />
+          <label htmlFor="netBanking">Net Banking</label>
+          <input type="radio" name="payment_option" id="cod" />
+          <label htmlFor="cod">Cash On Delivery</label>
+        </div>
+        <div className={styles.orderSummary}>
+          <h2>Order Summary</h2>
+          {cart.map((x, i) => {
+            return (
+              <div key={i}>
+                <p>{x.title}</p>
+                <p>x 1</p>
+                <p>{`₹ ${Math.round(x.price * 87.62)}`}</p>
+              </div>
+            );
+          })}
+          <p className={styles.orderTotal}>{`Total: ₹ ${findTotal()}`}</p>
+          <p className={styles.terms}>
+            By clicking 'Pay Now', you confirm that you have reviewed your order
+            and agree to our Terms & Conditions.
+          </p>
+          <button className={styles.payBtn}>Pay Now</button>
+        </div>
+      </div>
+      <div className={styles.closeIcon} onClick={() => change(!state)}>
+        <img src={cross} alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default Payment;
